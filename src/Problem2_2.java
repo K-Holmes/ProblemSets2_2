28b62a07a50007dcd408ad35f3900a98579009ef
Kayla Holmes <Kayla.Holmes@cdpipelinedevshop>
2022-08-16 09:51:01 -0400

    "Finished Activity"

---
+++ src/Problem2_2.java
@@ -0,0 +1,16 @@
+public class Problem2_2 {
+    public static void main(String[] args) {
+        //Use print statements to create your own beautiful original ASCII art.
+        //Use comments to describe what your image is depicting.
+
+        //Backside of a house with a triangular roof that hangs over the edge
+        System.out.println("   XX");
+        System.out.println("  X  X");
+        System.out.println(" X    X");
+        System.out.println("XXXXXXXX");
+        System.out.println(" X    X");
+        System.out.println(" X    X");
+        System.out.println(" X    X");
+        System.out.println(" XXXXXX");
+    }
+}
